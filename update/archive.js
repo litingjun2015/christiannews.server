956e1622d7cba3b40a5cc396fe162ed4805397ae
litingjun <tingjun.li@outlook.com>
2016-05-16 10:11:57 +0800

    prepare archive

--- update/archive.js
+++ update/archive.js
@@ -62,66 +62,77 @@ async.series([
   },
   //
   //// 保存文章列表
-  //function (done) {
-  //  async.eachSeries(Object.keys(articleList), function (classId, next) {
-  //    debug("classId: " + classId);
-  //    debug("articleList[classId]: " + articleList[classId]);
-  //    save.articleList(classId, articleList[classId], next);
-  //  }, done);
-  //},
+  function (done) {
+    async.eachSeries(Object.keys(articleList), function (classId, next) {
+      debug("classId: " + classId);
+      debug("articleList[classId]: " + articleList[classId]);
+      save.articleList(classId, articleList[classId], next);
+    }, done);
+  },
+
   //
-  ////
-  ////// 保存文章数量
-  //function (done) {
-  //  async.eachSeries(Object.keys(articleList), function (classId, next) {
-  //    save.articleCount(classId, articleList[classId].length, next);
-  //  }, done);
-  //},
-  //
-  ////// 重新整理文章列表，把重复的文章去掉
-  //function (done) {
-  //  debug('整理文章列表，把重复的文章去掉');
-  //
-  //  var articles = {};
-  //  Object.keys(articleList).forEach(function (classId) {
-  //    articleList[classId].forEach(function (item) {
-  //      articles[item.id] = item;
-  //    });
-  //  });
-  //
-  //  articleList = [];
-  //  Object.keys(articles).forEach(function (id) {
-  //    articleList.push(articles[id]);
-  //  });
-  //
-  //  done();
-  //},
-  //
-  ////// 依次读取文章的详细内容，并保存
-  //function (done) {
-  //  async.eachSeries(articleList, function (item, next) {
-  //    save.isAericleExists(item.id, function (err, exists) {
-  //      if (err) return next(err);
-  //
-  //      if (exists) {
-  //        debug('文章已存在：%s', item.url);
-  //        return next();
-  //      }
-  //
-  //      read.articleDetail(item.url, function (err, ret) {
-  //        if (err) return next(err);
-  //        save.articleDetail(item.id, ret.tags, ret.content, ret.time_text, function (err) {
-  //          if (err) return next(err);
-  //          save.articleTags(item.id, ret.tags, next);
-  //        });
-  //      });
-  //    });
-  //  }, done);
-  //}
+  //// 保存文章数量
+  function (done) {
+    async.eachSeries(Object.keys(articleList), function (classId, next) {
+      save.articleCount(classId, articleList[classId].length, next);
+    }, done);
+  },
+
+  //// 重新整理文章列表，把重复的文章去掉
+  function (done) {
+    debug('整理文章列表，把重复的文章去掉');
+
+    var articles = {};
+    Object.keys(articleList).forEach(function (classId) {
+      articleList[classId].forEach(function (item) {
+        articles[item.id] = item;
+      });
+    });
+
+    articleList = [];
+    Object.keys(articles).forEach(function (id) {
+      articleList.push(articles[id]);
+    });
+
+    done();
+  },
+
+  //// 依次读取文章的详细内容，并保存
+  function (done) {
+    async.eachSeries(articleList, function (item, next) {
+      save.isAericleExists(item.id, function (err, exists) {
+        if (err) return next(err);
+
+        if (exists) {
+          debug('文章已存在：%s', item.url);
+          return next();
+        }
+
+        read.articleDetail(item.url, function (err, ret) {
+          if (err) return next(err);
+          save.articleDetail(item.id, ret.tags, ret.content, ret.time_text, function (err) {
+            if (err) return next(err);
+            save.articleTags(item.id, ret.tags, next);
+          });
+        });
+      });
+    }, done);
+  }
 
 ], function (err) {
   if (err) console.error(err.stack);
 
-  console.log('完成');
-  process.exit(0);
+  db.query('delete FROM article_list where id not in (select id from article_detail)', function (err, data) {
+    if (err)
+    {
+      debug(err);
+      return callback(err);
+    }
+
+    debug(data);
+
+    console.log('完成');
+    process.exit(0);
+
+  });
 });
